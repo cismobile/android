f1b98b4e638e4233d341a8baa46d45da51bf7d4e
CHAN KOK AUN <kachan@solucis.com.my>
2013-04-16 16:01:52 +0800

    commint

--- example/app/view/Login.js
+++ example/app/view/Login.js
@@ -1,14 +1,7 @@
 Ext.define('mobileV1.view.Login', {
     extend: 'Ext.form.Panel',
     alias: "widget.loginview",
-    requires: [
-		'Ext.device.Connection',
-		'Ext.form.FieldSet', 
-		'Ext.form.Password', 
-		'Ext.Label', 
-		'Ext.Img', 
-		'Ext.util.DelayedTask'
-	],
+    requires: ['Ext.form.FieldSet', 'Ext.form.Password', 'Ext.Label', 'Ext.Img', 'Ext.util.DelayedTask','Ext.data.proxy.SessionStorage','Ext.field.Toggle'],
     config: {
         title: 'Login',
         items: [{
