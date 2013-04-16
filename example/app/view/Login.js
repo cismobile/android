89280a1070f7ac550c43d8fb4edd3c9f97251604
CHAN KOK AUN <kachan@solucis.com.my>
2013-04-16 15:44:19 +0800

    commint

--- example/app/view/Login.js
+++ example/app/view/Login.js
@@ -1,20 +1,13 @@
 Ext.define('mobileV1.view.Login', {
     extend: 'Ext.form.Panel',
     alias: "widget.loginview",
-     requires: [
-		'Ext.device.Connection',
-		'Ext.form.FieldSet', 
-		'Ext.form.Password', 
-		'Ext.Label', 'Ext.Img', 
-		'Ext.util.DelayedTask',
-		'Ext.field.Toggle'
-	],
+    requires: ['Ext.form.FieldSet', 'Ext.form.Password', 'Ext.Label', 'Ext.Img', 'Ext.util.DelayedTask','Ext.data.proxy.SessionStorage'],
     config: {
         title: 'Login',
         items: [{
                 xtype: 'image',
                 src: Ext.Viewport.getOrientation() == 'portrait' ? 'resources/image/login.png' : 'resources/image/login.png',
-                style: Ext.Viewport.getOrientation() == 'portrait' ? 'width:128px;height:128px;margin:auto;margin-top:20px' : 'width:40px;height:40px;margin:auto;margin-top:20px'
+                style: Ext.Viewport.getOrientation() == 'portrait' ? 'width:80px;height:80px;margin:auto;margin-top:50px' : 'width:40px;height:40px;margin:auto;margin-top:50px'
             }, {
                 xtype: 'label',
                 html: 'Login failed. Please enter the correct credentials.',
@@ -25,7 +18,8 @@ Ext.define('mobileV1.view.Login', {
                 style: 'color:#990000;margin:5px 0px;'
             }, {
                 xtype: 'fieldset',
-                style: 'margin-top:30px',
+                //title: 'Login Example',
+                style: 'margin-top:50px',
                 items: [{
                         xtype: 'textfield',
                         placeHolder: 'Username',
@@ -38,14 +32,7 @@ Ext.define('mobileV1.view.Login', {
                         itemId: 'passwordTextField',
                         name: 'passwordTextField',
                         required: true
-                    }, {
-						xtype: 'togglefield',
-						name: 'remember_me',
-						itemId: 'remember_me',
-						label: 'Remember me?',
-						value: 0,
-						labelWidth: 150
-					}
+                    }
                 ]
             }, {
                 xtype: 'button',
@@ -53,14 +40,7 @@ Ext.define('mobileV1.view.Login', {
                 ui: 'action',
                 padding: '10px',
                 text: 'Log In'
-            }, {
-				xtype: 'button',
-				itemId: 'cancelButton',
-				ui: 'cancel',
-				style: 'margin-top: 10px',
-				padding: '10px',
-				text: 'Cancel'
-			}
+            }
         ],
         listeners: [{
                 delegate: '#logInButton',
@@ -73,12 +53,10 @@ Ext.define('mobileV1.view.Login', {
         var me = this,
             usernameField = me.down('#userNameTextField'),
             passwordField = me.down('#passwordTextField'),
-			rememberField = me.down('#passwordTextField'),
             label = me.down('#signInFailedLabel'),
             username = usernameField.getValue(),
-            password = passwordField.getValue(),
-			remember = rememberField.getValue();
-
+            password = passwordField.getValue();
+		
         label.hide();
 
         // Using a delayed task in order to give the hide animation above
@@ -87,7 +65,7 @@ Ext.define('mobileV1.view.Login', {
 
             label.setHtml('');
 
-            me.fireEvent('signInCommand', me, username, password),remember;
+            me.fireEvent('signInCommand', me, username, password);
 
             usernameField.setValue('');
             passwordField.setValue('');
@@ -101,4 +79,5 @@ Ext.define('mobileV1.view.Login', {
         label.setHtml(message);
         label.show();
     }
-});
+
+})
